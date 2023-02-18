e502c577a98c1e6584adba2e61f62a8b802afa29
Demetrous-fd <dimaprodeus@yandex.ru>
2023-02-18 18:30:49 +0300

    Сжатие статики

--- AGOI/settings.py
+++ AGOI/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
