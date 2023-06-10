ab61d62686305aa1c89639f9a2ff67a2a4ff692d
Demetrous-fd <dimaprodeus@yandex.ru>
2023-06-10 18:21:18 +0300

    update backend script wait-for-postgres.sh

--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 set -e
+
 host="$1"
 shift
-cmd="$@"
-until PGPASSWORD="$DB_PASSWORD" psql -h "$host" -d "$DB_NAME" -U "$DB_USER" -c '\q';>
->&2 echo "Postgres is unavailable - sleeping"
-sleep 1
+
+until PGPASSWORD="$DB_PASSWORD" psql -h "$host" -d "$DB_NAME" -U "$DB_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
+
 >&2 echo "Postgres is up - executing command"
-exec $cmd
+exec "$@"
